5f52989fb69624c4ed74701362451afcee5a6590
Marc Veens <marcveens@hotmail.com>
2023-06-27 23:46:15 +0200

    Removed comments

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { getAllPosts, getPostBySlug } from '@/api/api';
+import { getPostBySlug } from '@/api/api';
 import { type BlogPost } from '@/types/BlogPost';
 import { markdownToHtml } from '@/utils/markdownToHtml';
 
@@ -17,32 +17,3 @@ const BlogPost = async (props: BlogPostProps) => {
 };
 
 export default BlogPost;
-
-// export async function getStaticProps({ params }: Params) {
-//   const post = getPostBySlug(params.slug, ['title', 'date', 'slug', 'description', 'content', 'category', 'tags']);
-//   const content = await markdownToHtml(post.content || '');
-
-//   return {
-//     props: {
-//       post: {
-//         ...post,
-//         content
-//       }
-//     }
-//   };
-// }
-
-// export async function getStaticPaths() {
-//   const posts = getAllPosts(['slug']);
-
-//   return {
-//     paths: posts.map((post) => {
-//       return {
-//         params: {
-//           slug: post.slug
-//         }
-//       };
-//     }),
-//     fallback: false
-//   };
-// }
