658a59a147b7d1e883290f88efcb6bec3f96fa63
Marc Veens <marcveens@hotmail.com>
2023-06-29 20:29:22 +0200

    Added GA4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { config } from '@/config/config';
 import '../styles/global.css';
 import { themeClass } from '@/styles/theme.css';
+import Script from 'next/script';
 
 export const metadata = {
   title: config.title,
@@ -11,6 +12,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={themeClass}>{children}</body>
+
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-RKSZWTWXW5" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-RKSZWTWXW5');
+        `}
+      </Script>
     </html>
   );
 }
